a9df9bcfb7c08f9f830ce73cd5b580c834871b9e
Juanfran <TRDQubix35@gmail.com>
2022-11-28 16:39:17 +0100

    frontend-games-rankings

--- app_index/urls.py
+++ app_index/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from app_index.views import inicio
+from app_index.views import *
 
 
 urlpatterns = [
     path('', inicio, name="inicio"),
-    
+    path('juegos', juegos, name="juegos"),
+    path('rankings', rankings, name="rankings"),
 ]
--- app_index/views.py
+++ app_index/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request, "index.html")
+
+def juegos(request):
+    return render(request, "juegos.html")
+
+def rankings(request):
+    return render (request, "rankings.html")
