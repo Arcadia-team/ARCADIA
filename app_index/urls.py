aec603467e87fd90d3cd04408532d42305ee558c
Marti <fmarti001@gmail.com>
2022-11-29 19:34:13 +0100

    css login

--- app_index/urls.py
+++ app_index/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from app_index.views import inicio
+from app_index.views import inicio, login
 
 
 urlpatterns = [
     path('', inicio, name="inicio"),
+    path('login/', login, name="login"),
+
     
 ]
--- app_index/views.py
+++ app_index/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request, "index.html")
+
+
+# Login
+def login(request):
+    return render(request, "login.html")
