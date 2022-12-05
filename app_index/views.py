edf070ed1561c1a56c21bc64b55b00f551b0c1ba
Marti <fmarti001@gmail.com>
2022-12-05 17:36:03 +0100

    subcarpetas-static-templates

--- app_index/views.py
+++ app_index/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def inicio(request):
-    return render(request, "index.html")
+    return render(request, "app_index/index.html")
 
 # Login
 def login(request):
-    return render(request, "login.html")
+    return render(request, "app_index/login.html")
 
 # Register
 def signup(request):
-    return render(request, "signup.html")
+    return render(request, "app_index/signup.html")
