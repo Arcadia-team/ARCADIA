3d96c9a66954098e03c35e43898214d5ae08bdf1
Juanfran <TRDQubix35@gmail.com>
2022-12-14 16:01:19 +0100

    Solución de bugs en rankings y ranking

--- app_index/urls.py
+++ app_index/urls.py
@@ -4,6 +4,7 @@ from app_index.views import *
 
 urlpatterns = [
     path('', inicio, name="inicio"),
+    path('inicio', inicio, name="inicio"),
     path('juegos', juegos, name="juegos"),
     path('rankings', rankings, name="rankings"),
     path('ranking', ranking, name="ranking"),
