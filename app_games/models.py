fc529854f68fdcdba3fc02b16cbf8992c14e4759
Marti <fmarti001@gmail.com>
2023-04-11 17:26:30 +0200

    migrate Game, Score y user_profile

--- app_games/models.py
+++ app_games/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Score(models.Model):
+    user_profile = models.ForeignKey('app_perfiles.UserProfile', on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    date_played = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user_profile.user.username} - {self.game.name}: {self.score}"
--- app_perfiles/models.py
+++ app_perfiles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from app_games.models import Game, Score
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='profile_photos/')
+
+    # Otros campos adicionales para el perfil
+    # Ejemplo:
+    bio = models.TextField(max_length=500, blank=True)
+    website = models.URLField(max_length=100, blank=True)
+    scores = models.ManyToManyField(Game, through=Score, related_name='user_profiles')
+
+    def __str__(self):
+        return self.user.username
