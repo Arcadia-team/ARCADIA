bdb127741465f751c74d8eb22042c80a1187a4e8
Marti <fmarti001@gmail.com>
2023-04-12 16:15:28 +0200

    avatar model

--- app_perfiles/models.py
+++ app_perfiles/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from app_games.models import Game, Score
 
+
+class Avatar(models.Model):
+    ruta_imagen = models.CharField(max_length=100)
+    
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to='profile_photos/')
+    avatar = models.ForeignKey(Avatar, on_delete=models.CASCADE, default=1)
 
     # Otros campos adicionales para el perfil
     # Ejemplo:
@@ -14,3 +19,5 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
