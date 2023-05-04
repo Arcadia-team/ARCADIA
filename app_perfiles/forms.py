2f573d7d0838b767bc17ec7e0b7ee2dc1d97a960
Marti <fmarti001@gmail.com>
2023-05-04 15:43:03 +0200

    username

--- app_perfiles/forms.py
+++ app_perfiles/forms.py
@@ -12,7 +12,8 @@ class AvatarChoiceField(forms.ModelChoiceField):
 
 class EditProfileForm(forms.ModelForm):
     avatar = AvatarChoiceField(queryset=Avatar.objects.all(), label='Avatar', empty_label=None, to_field_name='ruta_imagen', widget=forms.Select(attrs={'class': 'form-control'}))
-    
+    username = forms.CharField(max_length=30, label='Nombre de usuario', widget=forms.TextInput(attrs={'class': 'form-control'}))
+
     class Meta:
         model = UserProfile
         fields = ('avatar', 'bio', 'website',)
@@ -20,3 +21,17 @@ class EditProfileForm(forms.ModelForm):
             'bio': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
             'website': forms.URLInput(attrs={'class': 'form-control'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].initial = self.instance.user.username
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        user = instance.user
+        user.username = self.cleaned_data['username']
+        if commit:
+            instance.save()
+            user.save()
+        return instance
+ 
