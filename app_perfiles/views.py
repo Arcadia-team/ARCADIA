ac2ee217b9589ab544751123d4f8326c051f66f1
Marti <fmarti001@gmail.com>
2023-05-04 16:15:12 +0200

    guardar

--- app_perfiles/views.py
+++ app_perfiles/views.py
@@ -24,5 +24,22 @@ def edit_profile(request):
     return render(request, 'app_perfiles/editar.html', {'form': form})
     
 
+#formulario para actualizar
+from app_perfiles.forms import UserUpdateForm
+from django.contrib.auth.models import User
+from django.views.generic import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# Create your views here.
+
+#Actualizar info user:
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserUpdateForm
+    success_url =  reverse_lazy('inicio')
+    template_name = 'app_perfiles/user_pannel.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
 
 
