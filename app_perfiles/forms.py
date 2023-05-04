c5cfebebcdf68f9ecdcc3520a31a64453cfd1a45
Marti <fmarti001@gmail.com>
2023-05-04 15:59:15 +0200

    guarda

--- app_perfiles/forms.py
+++ app_perfiles/forms.py
@@ -35,3 +35,24 @@ class EditProfileForm(forms.ModelForm):
             user.save()
         return instance
  
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    avatar = forms.IntegerField()  # Agrega campos adicionales si es necesario
+
+    
+    def __init__(self, user_instance=None, user_profile_instance=None, *args, **kwargs):
+        super(UserUpdateForm, self).__init__(*args, **kwargs)
+
+        if user_instance:
+            self.fields['username'].initial = user_instance.username
+            self.fields['email'].initial = user_instance.email
+            self.fields['password'].initial = user_instance.password
+
+        if user_profile_instance:
+            self.fields['avatar'].initial = user_profile_instance.avatar
