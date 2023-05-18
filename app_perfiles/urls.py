159d535a88a6d0fa7bad7fcab45569323717b780
Marti <fmarti001@gmail.com>
2023-05-18 17:38:12 +0200

    pannel

--- app_perfiles/urls.py
+++ app_perfiles/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from app_perfiles.views import edit_profile, UserUpdateView
+from app_perfiles.views import UserUpdateView
 
 urlpatterns = [
-    path('', edit_profile, name="profile"),
     path('account/', UserUpdateView.as_view(), name="account"),
 
 ]
