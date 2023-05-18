a8c77bbcb2a4c351953c814ba89e1c88ba4e7b50
Marti <fmarti001@gmail.com>
2023-05-18 17:49:46 +0200

    view

--- app_perfiles/views.py
+++ app_perfiles/views.py
@@ -7,20 +7,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import EditProfileForm
 
-#Actualizar info profile:
-@login_required
-def edit_profile(request):
-    profile = request.user.userprofile
-    if request.method == 'POST':
-        form = EditProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            profile = form.save(commit=False)
-            profile.avatar = form.cleaned_data['avatar']
-            profile.save()
-            return redirect('profile')
-    else:
-        form = EditProfileForm(instance=profile)
-    return render(request, 'app_perfiles/editar.html', {'form': form})
+
     
 
     
@@ -34,8 +21,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class UserUpdateView(LoginRequiredMixin, UpdateView):
     model = User
     form_class = UserUpdateForm
-    success_url =  reverse_lazy('inicio')
     template_name = 'app_perfiles/user_pannel.html'
+    success_url =  reverse_lazy('account')
 
     def get_object(self, queryset=None):
         return self.request.user
